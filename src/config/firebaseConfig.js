ac202a54a5032e362262a6b851fb1a7a1321a327
Satyanarayan-kurmi <snkurmi975321@gmail.com>
2023-01-26 16:21:19 +0530

    crypto_tracker

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCNhq_GJDy8fuA0PVK2k4OAwL_UXxqDhfI",
+  authDomain: "crypto-tracker-c94ce.firebaseapp.com",
+  projectId: "crypto-tracker-c94ce",
+  storageBucket: "crypto-tracker-c94ce.appspot.com",
+  messagingSenderId: "433284800183",
+  appId: "1:433284800183:web:ce5bf3f8ea802faeedb732"
+};
+
+export default firebaseConfig;
